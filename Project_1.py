de56bc20ac89a8e55575e2937b0d36b5b8d697d4
Debbie Chang <debbiechang0531@gmail.com>
2018-01-28 22:43:17 -0500

    test reading the data

--- Project_1.py
+++ Project_1.py
@@ -8,17 +8,32 @@ customerId = '5a5992d95eaa612c093b0c0a'
 apiKey = '4a887901314de2781ea7ef954aca19e8'
 
 url = 'http://api.reimaginebanking.com/enterprise/accounts?key=4a887901314de2781ea7ef954aca19e8'.format(customerId,apiKey)
-payload =  {
-  "_id": "string"
+
+payload = {
+  "type": "Credit Card",
+  "bill_ids": [
+    "string"
+  ],
+  "_id": "string",
+  "rewards": 0,
+  "customer_id": "string",
+  #"nickname": "string",
+  "balance": 0
 }
 # Create a Savings Account
 response = requests.get(
 	url,
-	#data=json.dumps(payload),
+	data=json.dumps(payload),
 	headers={'content-type':'application/json'},
 	)
 
 if response.status_code == 200:
 	print('success')
 
-#print(response.content)
+data = response.json()
+print(data["results"][:10])
+
+#for i in range(0,50):
+#   print(data["results"][i])
+
+print(len(data["results"][:3]))
